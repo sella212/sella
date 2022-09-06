7d1f0a2fc11950e4be4d70bbb1bfcd375ea56ada
sella212 <112919006+sella212@users.noreply.github.com>
2022-09-06 10:16:14 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "cukong.py",
+    version = "0.1",
+    py_modules = ['cukong'],
+    url = "https://github.com/cukai212/ckong.git",
+    author = "cukai212",
+    install_requires = ["pyngrok"]
+)
